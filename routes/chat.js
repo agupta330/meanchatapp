4ae2ca33c4589028060d9ccd3f3964bc2558dd43
Aman Gupta <agupta330@csc.com>
2017-10-29 10:23:12 +0530

    changes

--- routes/chat.js
+++ routes/chat.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+// var server = require('http').createServer(app);
+// var io = require('socket.io')(server);
 var Chat = require('../models/Chat.js');
 
-server.listen(3001);
+// server.listen(3001);
 
-// socket io
-io.on('connection', function (socket) {
-  console.log('User connected');
-  socket.on('disconnect', function() {
-    console.log('User disconnected');
-  });
-  socket.on('save-message', function (data) {
-    console.log(data);
-    io.emit('new-message', { message: data });
-  });
-});
+// // socket io
+// io.on('connection', function (socket) {
+//   console.log('User connected');
+//   socket.on('disconnect', function() {
+//     console.log('User disconnected');
+//   });
+//   socket.on('save-message', function (data) {
+//     console.log(data);
+//     io.emit('new-message', { message: data });
+//   });
+// });
 
 /* GET ALL CHATS */
 router.get('/:room', function(req, res, next) {
