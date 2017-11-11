a30319ddad4e7e8db399b4d8704295c24c7e8907
Aman Gupta <agupta330@csc.com>
2017-11-11 17:12:43 +0530

    disable scrolling changes

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -35,7 +35,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\nbody,\r\nhtml{\r\n   overflow-x: hidden;\r\n   margin: 0;\r\n   padding: 0;\r\n}", ""]);
 
 // exports
 
